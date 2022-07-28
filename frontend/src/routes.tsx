a7e91c64d7431b3ad35c2c1fd268ccbf731b1c3e
Elizabete Fabri <elizabete958@gmail.com>
2022-07-28 16:50:13 -0300

    OrphanagesMap

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
+import OrphanagesMap from "./pages/OrphanagesMap";
 
 export default function MainRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/app" element={<OrphanagesMap />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
